0d4ec3cf91946fd1166d5e6785507e7c9526688f
marcvthooft <marcvthooft@gmail.com>
2014-01-17 20:32:25 +0100

    Initial Commit

---
+++ ToDoList/lolAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  lolAppDelegate.h
+//  ToDoList
+//
+//  Created by Marc van 't Hooft on 17-01-14.
+//  Copyright (c) 2014 Epesi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface lolAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Marc van 't Hooft on 17-01-14.
+//  Copyright (c) 2014 Epesi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "lolAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([lolAppDelegate class]));
+    }
+}
